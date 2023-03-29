0eb1c097a503efe6cd0160b55cb0fec6126bab56
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-03-29 03:31:56 +0300

    video container style fixes

--- src/js/video-thumbnail.js
+++ src/js/video-thumbnail.js
@@ -1,28 +1,19 @@
-
-//not workink
-// const iframe = document.querySelector('.how-its-made-iframe');
-// const picture = document.querySelector('.hitm-picture');
-
-// iframe.addEventListener('click', () => {
-//   picture.style.display = 'none';
-// });
-
-
-//working
 const picture = document.querySelector('.hitm-picture');
 var myConfObj = {
-  iframeMouseOver : false
-}
-window.addEventListener('blur',function(){
-  if(myConfObj.iframeMouseOver){
-      picture.style.display = 'none';
-      
+  iframeMouseOver: false,
+};
+window.addEventListener('blur', function () {
+  if (myConfObj.iframeMouseOver) {
+    picture.style.display = 'none';
   }
 });
-
-document.querySelector('.how-its-made-iframe').addEventListener('mouseover',function(){
-   myConfObj.iframeMouseOver = true;
-});
-document.querySelector('.how-its-made-iframe').addEventListener('mouseout',function(){
+document
+  .querySelector('.how-its-made-iframe')
+  .addEventListener('mouseover', function () {
+    myConfObj.iframeMouseOver = true;
+  });
+document
+  .querySelector('.how-its-made-iframe')
+  .addEventListener('mouseout', function () {
     myConfObj.iframeMouseOver = false;
-});
+  });
